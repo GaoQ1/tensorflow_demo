cf777942ab4f429eea685431273d24c4bdd1e797
jtl-gaoquan <gaoquan@goldmantis.com>
2017-07-10 20:02:53 +0800

    add cnn

--- cnn.py
+++ cnn.py
@@ -1,1 +1,19 @@
-#  26
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+
+# number 1 to 10 data
+mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+
+def compute_accuracy(v_xs, v_ys):
+    global prediction
+    y_pre = sess.run(prediction, feed_dict={xs: v_xs, keep_prob: 1})
+    correct_prediction = tf.equal(tf.argmax(y_pre, 1), tf.argmax(v_ys, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    result = sess.run(accuracy, feed_dict={xs: v_xs, ys: v_ys, keep_prob: 1})
+    return result
+
+def weight_variable(shape):
+    pass
+
+def bias_variable(shape):
+    pass
