bfcbd18600255abeb2d7ff98c2f427db9ffb5722
jtl-gaoquan <gaoquan@goldmantis.com>
2017-07-15 13:14:22 +0800

    edit 32

--- rnn.py
+++ rnn.py
@@ -8,11 +8,10 @@ mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 lr = 0.001
 training_iters = 100000
 batch_size = 128
-display_step = 10
 
 n_inputs = 28
 n_steps = 28
-n_hidden_unit = 128
+n_hidden_unis = 128
 n_classes = 10
 
 # tf Graph input
@@ -20,4 +19,43 @@ x = tf.placeholder(tf.float32, [None, n_steps, n_inputs])
 y = tf.placeholder(tf.float32, [None, n_classes])
 
 
-# 32
+# Define weights
+weights = {
+    'in': tf.Variable(tf.random_normal([n_inputs, n_hidden_unis])),
+    'out': tf.Variable(tf.random_normal([n_hidden_unis, n_classes]))
+}
+biases = {
+    'in': tf.Variable(tf.constant(0.1, shape=[n_hidden_unis,])),
+    'out': tf.Variable(tf.constant(0.1, shape=[n_classes,]))
+}
+
+def RNN(X, weights, biases):
+
+    results = None
+    return results
+
+
+pred = RNN(x, weights, biases)
+cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(pred, y))
+train_op = tf.train_AdamOptimizer(lr).minimize(cost)
+
+correct_pred = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+
+init = tf.global_variables_initializer()
+with tf.Session() as sess:
+    sess.run(init)
+    step = 0
+    while step * batch_size < training_iters:
+        batch_xs, batch_ys = mnist.train.next_batch(batch_size)
+        batch_xs = batch_xs.reshape([batch_size, n_steps, n_inputs])
+        sess.run([train_op], feed_dict={
+            x: batch_xs,
+            y: batch_ys
+        })
+        if step % 20 == 0:
+            print(sess.run(accuracy, feed_dict={
+                x: batch_xs,
+                y: batch_ys
+                }))
+        step += 1
